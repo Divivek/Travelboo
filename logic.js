4b89411a18bc2ca45160e9fae9cc3333270c1b64
Vivek Jain <Vivek Jain>
2017-05-19 16:41:02 -0400

    Comments clean up and removed dead code

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 
 var logEmail = "";  // Go
+
 $(document).ready(function() {
   console.log("JS ready!");
   //Hide the other pages
@@ -128,87 +129,71 @@ console.log("logged in Boolean? " + loggedIn);
 // ************************************************V**V**V**V**V**V**V********************************************
 
 //////////////////////////////////////DIVYAS JAVASCRIPT//////////////////////////////////////////////////////////
-//var defaultApp = firebase.initializeApp(config);
-//       console.log(defaultApp.name);  // "[DEFAULT]"
-//       //  create the database variable
-//       database = firebase.database();
-//       // var to access firebase storage API
-      var defaultStorage = firebase.storage();
-//       // create a ref to the storage to perform API function
-      var storageRef = defaultStorage.ref();
-      var place = "";
-      var localImageUrl = "";
-      var nameData="";
-      var commentdata = "";
-      var comment = $("#comment" );
-      var photos=$("#addPicBox");
- pageNumber = 1;      
- console.log(pageNumber)
-// //=================================================================================//
-// // adding a listener to the file upload button to start uploading files selected
-fileUploadButton.addEventListener('change', handleFileSelect, false);
-  // blow function handle uploading the files to the firebase storage
-  function handleFileSelect(e) {
-      //  extract the file handle
-      var file = e.target.files[0];
-      // Create the file metadata
-      var metadata = {  contentType: 'image/jpeg'};
-      // Upload file and metadata to the object 'image'
-      var uploadTask = storageRef.child('images/' + file.name  ).put(file, metadata);
-      // Listen for state changes, errors, and completion of the upload.
+var defaultStorage = firebase.storage();   // var to access firebase storage API
+var storageRef = defaultStorage.ref();    // create a ref to the storage to perform API function
+var place = "";
+var localImageUrl = "";
+var commentdata = "";
+var debug = false;
+
+function logwrite(message){
+  if (debug) { console.log(message); }
+}
+
+// adding a listener to the file upload button to start uploading files selected
+fileUploadButton.addEventListener('change', handleFileSelect, false); // Execute handleFileSelect functoin when button is clicked 
+// blow function handle uploading the files to the firebase storage
+function handleFileSelect(e) {
+    var file = e.target.files[0];   //  extract the file handle
+    var metadata = {  contentType: 'image/jpeg'}; // Create the file metadata
+    var uploadTask = storageRef.child('images/' + file.name  ).put(file, metadata);   // Upload file and metadata to the object 'image'
+ // Listen for state changes, errors, and completion of the upload.
     
-  uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED, // or 'state_changed'
     function(snapshot) {
-      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        // console.log('Upload is ' + progress + '% done');
-      switch (snapshot.state) {
-      case firebase.storage.TaskState.PAUSED: // or 'paused'
-        console.log('Upload is paused');
-      break;
-      case firebase.storage.TaskState.RUNNING: // or 'running'
-        console.log('Upload is running');
-      break;
-      }
-   
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        logwrite('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            logwrite('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            logwrite('Upload is running');
+            break;
+        } // End of Switch 
+     
     }, function(error) {
-      // A full list of error codes is available at
-      // https://firebase.google.com/docs/storage/web/handle-errors
-      switch (error.code) {
-      case 'storage/unauthorized':
-          // User doesn't have permission to access the object
-      console.log('Upload error: ' +  error.code);
-      break;
-      case 'storage/canceled':
-      console.log('Upload error: ' +  error.code);
-      // User canceled the upload
-      break;
-       // ...
-      case 'storage/unknown':
-        console.log('Upload error: ' +  error.code);
-        // Unknown error occurred, inspect error.serverResponse
-      break;
-      }
+        // A full list of error codes is available at  https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case 'storage/unauthorized':
+            logwrite('Upload error: ' +  error.code); // User doesn't have permission to access the object
+            break;
+          case 'storage/canceled':
+            logwrite('Upload error: ' +  error.code); // User canceled the upload
+            break;
+            // ... and more error can be added here  
+          case 'storage/unknown':
+            logwrite('Upload error: ' +  error.code); // Unknown error occurred, inspect error.serverResponse
+            break;
+        }
     }, function() {
       // Upload completed successfully, now we can get the download URL
       localImageUrl = uploadTask.snapshot.downloadURL;
-      console.log(comment);
-      database.ref().push({
+      database.ref().push({  // save newly generated Image URL and logged in user Email in the same record 
         userId : logEmail,
         imageUrl: localImageUrl,
+      });   // End of Push  
+     }); // End on uploadtast.on  
+ }; // end of handleFileSelect
 
-      }); 
-     });
-}
-
-$(document).on("click", ".addData", function(event) {
-  console.log("comment click working");
-      event.preventDefault();
-      comment = $("#comment").val();
-      var newComments = this.parentNode.childNodes[1].childNodes[1].value;
-      ldbKey = $(this).attr("dbKey");
-      console.log(comment);
-      console.log(ldbKey);
+$(document).on("click", ".userCommentsButton", function(event) {
+      logwrite("new comments added");
+      event.preventDefault(); // Prevent extra clicks 
+      var newComments = this.parentNode.childNodes[1].childNodes[1].value; //button.div.form.textarea.value
+      ldbKey = $(this).attr("dbKey");   // Extraxct the DB record key 
+      logwrite(newComments);
+      logwrite(ldbKey);
       database.ref(ldbKey).update({
       comment: newComments,
       });
@@ -216,60 +201,50 @@ $(document).on("click", ".addData", function(event) {
 
 
 function loadUserData() {
+    database.ref().on("child_added", function(childSnapshot, prevChildKey){
+        var localUser = childSnapshot.val().userId;
+        if (localUser == logEmail ) {
+            var dbRefKey = childSnapshot.key;
+            // use above key as property of button so that we can get to it at time of update
+            var column = $('<div class="col-sm-3 col-md-">');
+            localImageUrl = childSnapshot.val().imageUrl;
 
-// var ref = database.ref();
-// ref.once("value")
-//   .then(function(snapshot) {
-//     var hasName = snapshot.hasChild("name"); // true
-//     var hasAge = snapshot.hasChild("age"); // false
-//   });
-
-database.ref().on("child_added", function(childSnapshot, prevChildKey){
-    var localUser = childSnapshot.val().userId;
-    if (localUser == logEmail ) 
-    {
-        var dbRefKey = childSnapshot.key;
-        // use above key as property of button so that we can get to it at time of update
-        var column = $('<div class="col-sm-3 col-md-">');
-        localImageUrl = childSnapshot.val().imageUrl;
-
-        var photoImage = $("<img >");
-        photoImage.addClass("pictures");
-        photoImage.attr("src", localImageUrl);
-        commentData = childSnapshot.val().comment;
-        var submitButton = $("<button>");
-        submitButton.addClass("addData");
-        submitButton.attr("dbKey", dbRefKey);
-        var commentForm = $('<form class="form-group"><label for="comment"></label><textarea class="form-control" rows="5" id="comment">' + commentData + '</textarea> </form>')
-        $("#commentBox").append(commentData);
-        column.append(photoImage);
-        column.append(commentForm);
-        column.append(submitButton);
-        $("#playArea").append(column);
-        $(".addData").text("+ comment");
-        photoImage.click(pictureClick);
-      } // END IF of USER ID 
- }, function(errorObject){
-   console.log(errorObject.code);
- });
-};
+            var photoImage = $("<img >");
+            photoImage.addClass("pictures");
+            photoImage.attr("src", localImageUrl);
+            commentData = childSnapshot.val().comment;
+            var submitButton = $("<button>");
+            submitButton.addClass("userCommentsButton");
+            submitButton.attr("dbKey", dbRefKey);
+            var commentForm = $('<form class="form-group"><label for="comment"></label><textarea class="form-control" rows="5" id="comment">' + commentData + '</textarea> </form>')
+            $("#commentBox").append(commentData);
+            column.append(photoImage);
+            column.append(commentForm);
+            column.append(submitButton);
+            $("#playArea").append(column);
+            $(".userCommentsButton").text("+ comment");
+            photoImage.click(pictureClick);
+        } // END IF of USER ID 
+     }, function(errorObject){
+       logwrite(errorObject.code);
+     });
+    };
 
 function pictureClick(){
-     console.log("hidden");
-     console.log("next page shown");
+   logwrite("hidden");
+   logwrite("next page shown");
    $("#travel-Book").hide();
    $("#trip-details").show();
  }
- $("#back").on("click",function(){
+   $("#back").on("click",function(){
    $("#travel-Book").show();
    $("#trip-details").hide();
-   });
-
-
-
- ////////////////////////////////////////////END OF DIVYAS JAVASCRIPT/////////////////////////////////
+});
 
 });
+
+////////////////////////////////////////////END OF DIVYAS JAVASCRIPT/////////////////////////////////
+
 
 ////////////////////////////////////////////CHIRAGS JAVASCRIPT/////////////////////////////////////////
 var googleAPIKey="AIzaSyDNoZFYABiy-Dagp1LvMG9KbsOH0yleoXI";
